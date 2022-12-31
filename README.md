f973752042231376aba4dda503f996c4057ca8d9
ACBennett7 <andrewbennettdev@gmail.com>
2022-12-31 13:59:01 -0500

    Create basic API

---
+++ README.md
@@ -0,0 +1,2 @@
+As of the initial commit this is highly WIP. I've spent most of my time as a developer using Java and C++, and I want to be more comfortable with
+Golang. This will eventually be a functional API once I've experimented with a few different versions.
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "fmt"
+    "github.com/gorilla/mux"
+    "io/ioutil"
+    "log"
+    "net/http"
+)
+
+func main() {
+    handleRequests()
+}
+
+func handleRequests() {
+    r := mux.NewRouter()
+    r.HandleFunc("/weather/{zip}", getCurrentWeather)
+    log.Fatal(http.ListenAndServe(":8089", r))
+}
+
+func getCurrentWeather(w http.ResponseWriter, r *http.Request){
+    vars := mux.Vars(r)
+    zip := vars["zip"]
+
+    url := "https://weatherapi-com.p.rapidapi.com/current.json?q=" + zip
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("X-RapidAPI-Key", "721a5b3fb4msh34887dec04bfaeap11029fjsn289a1e4b579b")
+	req.Header.Add("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Fprintf(w, string(body))
+}
