08103d71956b02da8aeaf1594ee08392874443ae
andrew.bennett4 <andrew.bennett@bettercloud.com>
2023-10-12 11:46:54 -0400

    remove idea folder

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
-	goconfig "github.com/iglin/go-config"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
+	goconfig "github.com/iglin/go-config"
 )
 
 var config = goconfig.NewConfig("./secretConfig.yaml", goconfig.Yaml)
@@ -15,7 +16,7 @@ var apiHost = config.GetString("data.apiHost")
 var apiKey = config.GetString("data.apiKey")
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
-func main() {	
+func main() {
 	handleRequests()
 }
 
@@ -27,36 +28,41 @@ func handleRequests() {
 }
 
 func getList(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Available Endpoints:\n -/current/{location}\n -/astronomy/{location}\n -/timezone/{location}\n -/sports/{location}")
+	fmt.Fprintf(w, "Available Endpoints:\n -/current/{location}\n -/astronomy/{location}\n -/timezone/{location}\n -/sports/{location}")
 }
 
 func getData(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	endpoint := vars["endpoint"]
 	location := vars["location"]
-	
+
 	url := "https://weatherapi-com.p.rapidapi.com/" + endpoint + ".json?q=" + location
 	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-	   log.Fatal(err)
-	}
-	req.Header.Add("X-RapidAPI-Key", apiKey )
-	req.Header.Add("X-RapidAPI-Host", apiHost)
-
-	res, err := myClient.Do(req)
-	if err != nil {
-	   log.Fatal(err)
-	}
-
-	defer res.Body.Close()
-	
-	target := new(InputData) 
-	err = json.NewDecoder(res.Body).Decode(target)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	transformedBody := Transform(target) // NOTE: this only works for /current/ right now
+	if apiKey == "secret" {
+		fmt.Fprintf(w, "%s", "You need an API key to call this endpoint!")
+	} else {
+		req.Header.Add("X-RapidAPI-Key", apiKey)
+		req.Header.Add("X-RapidAPI-Host", apiHost)
 
-	fmt.Fprintf(w, "%+v", transformedBody)
+		res, err := myClient.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer res.Body.Close()
+
+		target := new(InputData)
+		err = json.NewDecoder(res.Body).Decode(target)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		transformedBody := Transform(target) // NOTE: this only works for /current/ right now
+
+		fmt.Fprintf(w, "%+v", transformedBody)
+	}
 }
