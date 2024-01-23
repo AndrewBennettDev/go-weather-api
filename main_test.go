ec6799870ba83e862f9a7bd1b31be54ea9e237f6
AndrewBennettDev <andrewbennettdev@gmail.com>
2024-01-23 12:38:22 -0500

    add basic test case

--- main_test.go
+++ main_test.go
@@ -1,6 +1,25 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
-	"regexp"
 )
+
+func TestGetDataHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/{location}", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getData)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+}
